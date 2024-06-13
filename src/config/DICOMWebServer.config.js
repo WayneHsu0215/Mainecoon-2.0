ea42b90eda674a1d2d8da29695a9f1d81951f8e4
yaokaixuan2000 <143184442+yaokaixuan2000@users.noreply.github.com>
2024-06-13 10:57:41 +0800

    annGroup 1.0

--- src/config/DICOMWebServer.config.js
+++ src/config/DICOMWebServer.config.js
@@ -1,4 +1,23 @@
 export default {
+    // QIDO: {
+    //     enableHTTPS: true,
+    //     hostname: "development.j4care.com",
+    //     port: "11443",
+    //     pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+    //     Token: null,
+    // },
+    // WADO: {
+    //     enableHTTPS: true,
+    //     // hostname: "ditto.dicom.tw",
+    //     hostname: "development.j4care.com",
+    //     port: "11443",
+    //     URI_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+    //     RS_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+    //     Mode: "rs",
+    //     Token: null,
+    // },
+
+//     ditto
     QIDO: {
         enableHTTPS: true,
         hostname: "ditto.dicom.tw",
@@ -15,4 +34,23 @@ export default {
         Mode: "rs",
         Token: null
     }
+//     google
+//     QIDO: {
+//         enableHTTPS: true,
+//         hostname: "dicomwebproxy-bqmq3usc3a-uc.a.run.app",
+//         port: "",
+//         pathname: "dicomWeb",
+//         Token: null,
+//     },
+//     WADO: {
+//         enableHTTPS: true,
+//         hostname: "dicomwebproxy-bqmq3usc3a-uc.a.run.app",
+//         port: "",
+//         URI_pathname: "dicomWeb",
+//         RS_pathname: "dicomWeb",
+//         Mode: "rs",
+//         Token: null,
+//     },
 }
+
+
