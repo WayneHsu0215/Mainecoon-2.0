3280404c01da8fd0cce8dbde6e51a6d1305a810f
HsuTzu26 <winstonhsu26@gmail.com>
2024-07-12 10:23:25 +0800

    feat: docker-compose.yml and config template
    - create docker-compose file
    - add DICOMWebConfig.config.js template
    - - add the path of DICOMWebConfig.config.js to .gitignore

---
+++ src/config/DICOMWebServer.template.config.js
@@ -0,0 +1,22 @@
+export default {
+    PACS_NAME: {
+        QIDO: {
+            enableHTTPS: true,
+            hostname: "pacs server hostname",
+            port: "port",
+            pathname: "pacs server pathname",
+            Token: null
+        },
+        WADO: {
+            enableHTTPS: true,
+            hostname: "pacs server hostname",
+            port: "port",
+            URI_pathname: "pacs server WADO-URI pathname",
+            RS_pathname: "pacs server WADO-RS pathname",
+            Mode: "rs",
+            Token: null
+        }
+    },
+}
+
+
