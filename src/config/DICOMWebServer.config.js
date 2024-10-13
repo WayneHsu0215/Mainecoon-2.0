38654cb7168aef263e2783e5ccddd790c4cf6a5b
Hsu Chao-Wei <120437313+WayneHsu0215@users.noreply.github.com>
2024-10-13 17:11:34 +0800

    修正在沒有oauth的情況下搜尋頁面不會出現thubnail

--- src/config/DICOMWebServer.config.js
+++ src/config/DICOMWebServer.config.js
@@ -17,17 +17,17 @@ export default {
     //         Token: null
     //     }
     // },
-    DITTO: {
+    NewRaccoon: {
         QIDO: {
             enableHTTPS: true,
-            hostname: "ditto.dicom.tw",
+            hostname: "ditto-tmp.dicom.tw",
             port: "",
             pathname: "/dicom-web",
             Token: null
         },
         WADO: {
             enableHTTPS: true,
-            hostname: "ditto.dicom.tw",
+            hostname: "ditto-tmp.dicom.tw",
             port: "",
             URI_pathname: "/dicom-web/wado",
             RS_pathname: "/dicom-web",
@@ -35,76 +35,76 @@ export default {
             Token: null
         }
     },
-    PACS_MITW: {
-        QIDO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10000",
-            pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
-            Token: null
-        },
-        WADO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10000",
-            URI_pathname: "/dcm4chee-arc/aets/DCM4CHEE/wado",
-            RS_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
-            Mode: "rs",
-            Token: null
-        }
-    },
-    RACCOON_MITW: {
-        QIDO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10109",
-            pathname: "/dicom-web",
-            Token: null
-        },
-        WADO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10109",
-            URI_pathname: "/dicom-web/wado",
-            RS_pathname: "/dicom-web",
-            Mode: "rs",
-            Token: null
-        }
-    },
-    EBM_MITW: {
-        QIDO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10107",
-            pathname: "/DICOMwebRS/DICOMwebRS.dll",
-            Token: null
-        },
-        WADO: {
-            enableHTTPS: false,
-            hostname: "172.18.0.53",
-            port: "10107",
-            URI_pathname: "/DICOMwebRS/DICOMwebRS.dll",
-            RS_pathname: "/DICOMwebRS/DICOMwebRS.dll",
-            Mode: "rs",
-            Token: null
-        }
-    },
-    LOCAL_RACCOON_MITW: {
-        QIDO: {
-            enableHTTPS: false,
-            hostname: "127.0.0.1",
-            port: "8081",
-            pathname: "/dicom-web",
-            Token: null
-        },
-        WADO: {
-            enableHTTPS: false,
-            hostname: "127.0.0.1",
-            port: "8081",
-            URI_pathname: "/dicom-web/wado",
-            RS_pathname: "/dicom-web",
-            Mode: "rs",
-            Token: null
-        }
-    },
+    // PACS_MITW: {
+    //     QIDO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10000",
+    //         pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+    //         Token: null
+    //     },
+    //     WADO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10000",
+    //         URI_pathname: "/dcm4chee-arc/aets/DCM4CHEE/wado",
+    //         RS_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+    //         Mode: "rs",
+    //         Token: null
+    //     }
+    // },
+    // RACCOON_MITW: {
+    //     QIDO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10109",
+    //         pathname: "/dicom-web",
+    //         Token: null
+    //     },
+    //     WADO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10109",
+    //         URI_pathname: "/dicom-web/wado",
+    //         RS_pathname: "/dicom-web",
+    //         Mode: "rs",
+    //         Token: null
+    //     }
+    // },
+    // EBM_MITW: {
+    //     QIDO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10107",
+    //         pathname: "/DICOMwebRS/DICOMwebRS.dll",
+    //         Token: null
+    //     },
+    //     WADO: {
+    //         enableHTTPS: false,
+    //         hostname: "172.18.0.53",
+    //         port: "10107",
+    //         URI_pathname: "/DICOMwebRS/DICOMwebRS.dll",
+    //         RS_pathname: "/DICOMwebRS/DICOMwebRS.dll",
+    //         Mode: "rs",
+    //         Token: null
+    //     }
+    // },
+    // LOCAL_RACCOON_MITW: {
+    //     QIDO: {
+    //         enableHTTPS: false,
+    //         hostname: "127.0.0.1",
+    //         port: "8081",
+    //         pathname: "/dicom-web",
+    //         Token: null
+    //     },
+    //     WADO: {
+    //         enableHTTPS: false,
+    //         hostname: "127.0.0.1",
+    //         port: "8081",
+    //         URI_pathname: "/dicom-web/wado",
+    //         RS_pathname: "/dicom-web",
+    //         Mode: "rs",
+    //         Token: null
+    //     }
+    // },
 }
